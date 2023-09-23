9664193e021288b71ddd357d5f3b936145f8bdbe
burakkurt <krtburak@outlook.com>
2023-09-24 01:45:50 +0300

    feature: m Uİ

--- app/global/utilities.styled.ts
+++ app/global/utilities.styled.ts
@@ -4,5 +4,7 @@ export const Container = styled.div`
   border: 1px solid black;
   width: 80%;
   margin: auto;
+  padding: 10px;
   background-color: ${(props) => props.theme.backgrounds.main};
+  color: ${(props) => props.theme.colors.main};
 `
